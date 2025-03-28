a3715dfabb8bc85ac2593ab98931121d40d3d57d
SivaChandrasekharJavvadi <sivachandrasekharj.3@gmail.com>
2025-03-28 17:19:17 +0530

    Update

--- src/main/java/com.lowkey.movieservice/service/EmailService.java
+++ src/main/java/com.lowkey.movieservice/service/EmailService.java
@@ -1,12 +1,11 @@
 package com.lowkey.movieservice.service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
-
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
 
 @Service
 public class EmailService {
